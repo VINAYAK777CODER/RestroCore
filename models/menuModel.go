181f0c96451ca02d5af8349901ba11279aacf4eb
VINAYAK777CODER <4517vinayak12a1@gmail.com>
2025-12-04 03:44:48 +0530

    refactor: clean and correct Menu struct formatting and tags

--- models/menuModel.go
+++ models/menuModel.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Menu struct {
-    ID         primitive.ObjectID `bson:"_id"`
-    Name       string             `json:"name" validate:"required"`
-    Category   string             `json:"category" validate:"required"`
-    Start_Date *time.Time         `json:"start_date"`
-    End_Date   *time.Time         `json:"end_date"`
-    Created_at time.Time          `json:"created_at"`
-    Updated_at time.Time          `json:"updated_at"`
-    Menu_id    string             `json:"food_id"`
+    ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+    Name       string             `json:"name" validate:"required,min=2,max=50"`
+    Category   string             `json:"category" validate:"required,min=2,max=50"`
+    Start_Date *time.Time         `json:"start_date,omitempty" bson:"start_date,omitempty"`
+    End_Date   *time.Time         `json:"end_date,omitempty" bson:"end_date,omitempty"`
+    Created_at time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+    Updated_at time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+    Menu_id    string             `json:"menu_id" bson:"menu_id" validate:"required"`
 }

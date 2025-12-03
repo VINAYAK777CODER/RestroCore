321601addaca00a63dc8c264e741e1332b19d2d0
VINAYAK777CODER <4517vinayak12a1@gmail.com>
2025-12-04 03:46:54 +0530

    refactor: clean and correct Invoice  struct formatting and tags

--- models/invoiceModel.go
+++ models/invoiceModel.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Invoice struct {
-    ID               primitive.ObjectID `bson:"_id"`
-    Invoice_id       string             `json:"invoice_id"`
-    Order_id         string             `json:"order_id"`
-    Payment_method   *string            `json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
-    Payment_status   *string            `json:"payment_status" validate:"required,eq=PENDING|eq=PAID"`
-    Payment_due_date time.Time          `json:"Payment_due_date"`
-    Created_at       time.Time          `json:"created_at"`
-    Updated_at       time.Time          `json:"updated_at"`
+    ID               primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+    Invoice_id       string             `json:"invoice_id" bson:"invoice_id" validate:"required"`
+    Order_id         string             `json:"order_id" bson:"order_id" validate:"required"`
+    Payment_method   *string            `json:"payment_method,omitempty" bson:"payment_method,omitempty" validate:"omitempty,oneof=CARD CASH"`
+    Payment_status   *string            `json:"payment_status,omitempty" bson:"payment_status,omitempty" validate:"required,oneof=PENDING PAID"`
+    Payment_due_date time.Time          `json:"payment_due_date,omitempty" bson:"payment_due_date,omitempty"`
+    Created_at       time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+    Updated_at       time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
